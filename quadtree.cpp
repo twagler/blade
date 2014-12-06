1046f725aee516ae5a5137ee135331f6cce414aa
Lagree <alan.lagree@gmail.com>
2014-12-06 02:01:38 -0500

    started fleshing out more

---
+++ quadtree.cpp
@@ -0,0 +1,44 @@
+#include "quadtree.h"
+#include "lawnmap.h"
+
+QuadTree::QuadTree()
+{
+    //These QuadTree pointrs point to the parents and children of this quad tree
+    QuadTree* NW = nullptr;
+    QuadTree* NE = nullptr;
+    QuadTree* SW = nullptr;
+    QuadTree* SE = nullptr;
+    QuadTree* parent = nullptr;
+
+    //this int indicates which quadrant of the parent that this quadtree is in
+    //0=NW, 1=NE, 2=SW, 3=SE
+    int quadrant = -1;
+
+
+    //if we are at the leaf (bottom) level, these will not populated with valid lawncoordinate objects.  If we are at any level above the bottom level, these are null
+    LawnCoordinate leafNW = null;
+    LawnCoordinate leafNE = null;
+    LawnCoordinate leafSW = null;
+    LawnCoordinate leafSE = null;
+
+    //if the ENTIRE tree is mowed, this is 1.  else 0.
+    int mowed = -1;
+    //if there is ANY boundary in this tree, this is 1.  else 0
+    int boundary = -1;
+    //if there is ANY obstacle in this tree, this is 1.  else 0
+    int obstacle = -1;
+}
+/*
+ * builds the quadtree
+ */
+void QuadTree::build(std::vector<std::vector<LawnCoordinate>> vList) {
+    //need to assume that vList was created correctly
+    int x = 0;
+    int y = 0;
+
+    //we build out tree from the bottom up, one quad at a time.  So we look at our vList in 2x2 chunks.
+    //x starts at 0 but size is 1, plus need to check ahead one since we're grabbing 2 at a time.
+    while(x+2<vList.size()){
+
+    }
+ }
